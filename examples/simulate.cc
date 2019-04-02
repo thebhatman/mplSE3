e800c6ae0df3b53993ce90a9f0f44c87854e96db
kvmanohar22 <kvmanohar22@gmail.com>
2019-04-02 17:43:03 +0530

    fix typo

--- examples/simulate.cc
+++ examples/simulate.cc
@@ -14,11 +14,11 @@ using namespace sim;
 
 int main() {
     // Generate the scene (by default no objects are present)
-    Scene *scene = new Scene(1200, 900, "Simulator");
+    Scene scene(1200, 900, "Simulator");
 
     // Add cuboids
-    std::vector<Object*> &_objects = scene->mutable_cubes();
-    std::map<std::string, Texture2D> &_textures = scene->get_textures();
+    std::vector<Object*> &_objects = scene.mutable_cubes();
+    std::map<std::string, Texture2D> &_textures = scene.get_textures();
     _objects.push_back(
         new Object("cube1",
                    glm::vec3(-20.0f, 0.0f, 0.0f),  // center of cube
@@ -48,7 +48,7 @@ int main() {
         );
 
     // Add some points
-    std::vector<Point*> &_points = scene->mutable_points();
+    std::vector<Point*> &_points = scene.mutable_points();
     _points.push_back(
         new Point("point1",
                   glm::vec3(0.0f, 0.0f, 0.0f), // position
@@ -69,7 +69,7 @@ int main() {
         );
 
     // Add some lines
-    std::vector<Line*> &_lines = scene->mutable_lines();
+    std::vector<Line*> &_lines = scene.mutable_lines();
     _lines.push_back(
         new Line("line1",
                  glm::vec3(0.0f, 0.0f, 0.0f), // starting point
@@ -90,11 +90,11 @@ int main() {
         );
 
     // Render the scene in a separate thread
-    std::thread _thread = std::thread(&Scene::render, scene);
+    std::thread _thread = std::thread(&Scene::render, &scene);
 
     usleep(10000000);
 
-    scene->add_cube(
+    scene.add_cube(
         new Object("cube4",
                    glm::vec3(0.0f, 0.0f, 50.0f),
                    glm::vec3(5.0f, 5.0f, 5.0f),
